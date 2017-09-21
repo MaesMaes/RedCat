b429a0d80f12052f6c3a3721b7a75ea7631e67f2
MaesMaes <t.bitw.fl@yandex.ru>
2017-09-21 18:52:06 +0500

    Настраиваем маршруты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser'; //Работа с браузером
 import { NgModule } from '@angular/core'; //Декоратор который превращает класс в модуль
+import { BrowserModule } from '@angular/platform-browser'; //Работа с браузером
+
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component'; //Функциональность корневого компонента
+import { LoginComponent }   from './login.component'
+import { HomeComponent }   from './home.component'
+import { NotFoundComponent }   from './not-found.component';
 
-@NgModule({  //Декоратор
-  declarations: [AppComponent], //Классы представлений(компоненты, директивы, каналы)
-  imports: [BrowserModule], //Что требуется для работы шаблонов компонентов модуля
-  providers: [], //Классы, создающие сервисы
-  bootstrap: [AppComponent] //Корневой компонент, вызывается по умолчанию при загрузке приложения
+// определение маршрутов
+const appRoutes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '**', component: NotFoundComponent }
+];
+
+//Декоратор
+@NgModule({
+
+  //Классы представлений(компоненты, директивы, каналы)
+  declarations: [AppComponent, HomeComponent, LoginComponent, NotFoundComponent],
+
+  //Что требуется для работы шаблонов компонентов модуля
+  imports: [
+      BrowserModule,
+      RouterModule.forRoot(appRoutes)
+  ],
+
+  //Классы, создающие сервисы
+  providers: [],
+
+  //Корневой компонент, вызывается по умолчанию при загрузке приложения
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
